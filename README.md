0b1a3e6a009c04b27e98b05f6fe34366f4734bc2
bsp8649 <bsp8649@rit.edu>
2024-05-21 13:16:23 -0400

    Refactored page to use white mode, and changed parts of the page to be easier to read (page.tsx).

--- README.md
+++ README.md
@@ -4,78 +4,44 @@ Welcome to the Kibu Technical Project portion of the interview! Congratulations 
 
 ## Overview
 
-The admin tooling features of Kibu are a large part of our product. In this project you will create a basic implementation of viewing and creating notes for a user. You will create a basic react front end which connects to a simple backend containing users and notes.
+This is a simple notes taking and member recording application for the Kibu Technical Interview.
 
-## Specification
+## Libraries Used
+NextJs - setup and server components
+Axios - for interacting with the server using api calls
+React Query - for managing server state by fetching and caching data
 
-Your solution must include the following:
 
-1. Utilize React and TypeScript
-2. A page to list all users and their respective notes
-3. Ability to create a new note for a user
+## Design
 
-If time allows the following are nice to have features:
+I designed my application to be customer accessible., 
 
-1. Add a new field to note called `timestamp` which tracks the current time when saved
-2. Ability to update a note
-3. Add a new resource in the backend called `audit_log` which tracks changes when a note is updated
+## Run and Test
+To run:
 
-The following can be considered out of scope:
-
-1. Authorization, authentication, and security of the backend server
-
-## Backend Server
-
-To help with your implementation we have provided a way to easily use a backend server with the `json-server` package. First install the `json-server` package with
+Install the json server
 
 ```
 npm install -g json-server
 ```
 
-Then start the server using
+Install any other dependencies
 
 ```
-json-server --watch db.json
+npm i
 ```
 
-You should see the following output in your terminal
+Start the json server
 
 ```
-Index:
-http://localhost:3000/
+npm install -g json-server
+```
+This will host the json server on http://localhost:3000/.
+Open a new terminal, and run the application in the dev environment
 
-Static files:
-Serving ./public directory if it exists
-
-Endpoints:
-http://localhost:3000/members
-http://localhost:3000/notes
+```
+npm install dev
 ```
 
-This will create GET, POST, PUT, PATCH, and DELETE routes for both `/members` and `/notes`.
+Then open a browser and navigate to http://localhost:3001/, since 3000 is already hosting the json.
 
-You can learn more about `json-server` [here](https://www.npmjs.com/package/json-server/v/0.16.1)
-
-## Getting Started
-
-To complete this project you will not need to make any changes to the `json-server`. Therefore, to get started you should create a new React App. You can learn more from the [React Docs](https://react.dev/learn/start-a-new-react-project)
-
-## Scoring
-
-You will be scored on:
-
-1. Code functionality
-2. Code quality
-3. Thoughtful design and UI
-4. Documentation on any design decisions or libraries used
-
-Bonus points for:
-
-1. Using [NextJs](https://nextjs.org/docs) with server components
-2. Using [React Query](https://tanstack.com/query/latest/docs/framework/react/overview)
-
-## Submission
-
-You will submit your code via GitHub (or your git repository of preference) and email the link to [ben@kibuhq.com](mailto:ben@kibuhq.com). Your Git repo should include documentation on how to run your application locally.
-
-Feel free to reach out to ask followup questions or clarify any requirements.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+"use client"
 import React, { useState, useEffect } from 'react';
 import { getMembers, getMemberNotes, createNote, Member, Note } from './api';
 
 const MemberNotes: React.FC = () => {
+  //Defining state based constants to keep track of the current members, notes, and whether we are adding a note.
   const [members, setMembers] = useState<Member[]>([]);
   const [selectedMemberId, setSelectedMemberId] = useState<string>('');
   const [notes, setNotes] = useState<Note[]>([]);
   const [newNoteText, setNewNoteText] = useState<string>('');
+  const [isAddingNote, setIsAddingNote] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +19,7 @@ const MemberNotes: React.FC = () => {
     fetchData();
   }, []);
 
+  //Handler to change members, as we are using a dropdown. Will set the new notes accordingly after it fetches them with getMemberNotes, 
   const handleMemberChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
     const memberId = event.target.value;
     setSelectedMemberId(memberId);
@@ -23,9 +27,10 @@ const MemberNotes: React.FC = () => {
     setNotes(memberNotes);
   };
 
+  //function to create a new note.
+  //will not update if there is no selected member or n
   const handleCreateNote = async () => {
     if (!selectedMemberId || !newNoteText) return;
-
     try {
       await createNote(selectedMemberId, newNoteText);
       const updatedNotes = await getMemberNotes(selectedMemberId);
@@ -37,7 +42,7 @@ const MemberNotes: React.FC = () => {
   };
 
   return (
-    <div>
+    <div style={{ backgroundColor: 'white', color: 'black', padding: '1em', fontFamily: 'Arial, sans-serif'}}>
       <h1>Member Notes</h1>
       <div>
         <label htmlFor="memberSelect">Select a Member:</label>
@@ -51,22 +56,33 @@ const MemberNotes: React.FC = () => {
         </select>
       </div>
       <div>
-        <h2>Notes</h2>
         {selectedMemberId && (
-          <div>
-            <ul>
-              {notes.map((note) => (
-                <li key={note.id}>{note.text}</li>
-              ))}
-            </ul>
-            <textarea
-              value={newNoteText}
-              onChange={(e) => setNewNoteText(e.target.value)}
-              placeholder="Enter new note"
-            />
-            <button onClick={handleCreateNote}>Add Note</button>
+        <div>
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
+            {notes.map((note) => (
+              <li key={note.id} style={{ marginBottom: '1em' }}>{note.text}</li>
+            ))}
+          </ul>
+          <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1em' }}>
+            {isAddingNote ? (
+              <div style={{ width: '100%', marginRight: '0.5em' }}>
+                <textarea
+                  value={newNoteText}
+                  onChange={(e) => setNewNoteText(e.target.value)}
+                  placeholder="Enter new note"
+                  style={{ width: '100%', marginBottom: '1em' }}
+                />
+                <button onClick={handleCreateNote} style={{ outline: '2px solid black', background: 'none', padding: '0.5em 1em', borderRadius: '5px' }}>Add Note</button>
+              </div>
+            ) : (
+              <button onClick={() => setIsAddingNote(true)} style={{ background: 'none', padding: '0.5em 1em', borderRadius: '5px' }}>Add New Note</button>
+            )}
+            <button onClick={() => setIsAddingNote(!isAddingNote)} style={{ background: 'none', padding: '0.5em 1em', borderRadius: '5px', border: '2px solid black' }}>
+              {isAddingNote ? 'Hide Textbox' : 'Show Textbox'}
+            </button>
           </div>
-        )}
+        </div>
+      )}
       </div>
     </div>
   );
