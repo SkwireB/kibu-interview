2547d98e2eeebbd2b56501a448c078798617362d
bsp8649 <bsp8649@rit.edu>
2024-05-21 11:16:06 -0400

    Initial Setup For Notes and members

---
+++ src/app/Member.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export interface Member {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
+export function Member() {
+  return (
+    <div>
+    </div>
+  );
+}
